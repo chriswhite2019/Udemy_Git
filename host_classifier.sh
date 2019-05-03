4025240981cdebfb29de44a5e018b50ffd0ec889
chriswhite2019 <49655421+chriswhite2019@users.noreply.github.com>
2019-05-03 16:11:11 -0400

    updated

--- host_classifier.sh
+++ host_classifier.sh
@@ -1,62 +1,50 @@
 #!/bin/sh
+
 if [ "$#" -lt 1 ]; then
-echo "please provide at least 1 file"
+        echo "please provide at least 1 file"
 fi
 
-var1="0"
-var2="0"
-var3="0"
-var4="0"
-var5="0"
-var6="0"
+for each in $*; do
+        echo ""
+        echo File = $each
 
-for each in $*
-do 
-echo ""
-echo File = $each
+        os_windows_count="0"
+        os_linux_count="0"
+        os_other_count="0"
+        env_production_count="0"
+        env_development_count="0"
+        env_other_count="0"
 
 while read line; do
-env=`echo "$line" | cut -c6`
-os=`echo "$line" | cut -c7`
+        env=`echo "$line" | cut -c6`
+        os=`echo "$line" | cut -c7`
 
 
 if [ "$os" == 'w' ]; then
-((var1++))
+        ((os_windows_count++))
+elif [ "$os" == 'l' ]; then
+        ((os_linux_count++))
+else
+        ((os_other_count++))
 fi
 
-if [ "$os" == 'l' ]; then 
-((var2++)) 
-fi
-
-if [ "$os" != 'w' ] && [ "$os" != 'l' ]; then 
-((var3++)) 
-fi
-
-if [ "$env" == 'p' ]; then 
-((var4++)) 
-fi
-
-if [ "$env" == 'd' ]; then
-((var5++))
-fi
-
-if [ "$env" != 'p' ] && [ "$env" != 'd' ]; then
-((var6++))
+if [ "$env" == 'p' ]; then
+        ((env_production_count++))
+elif [ "$env" == 'd' ]; then
+        ((env_development_count++))
+else
+        ((env_other_count++))
 fi
 
 done < $each
 
-echo "OS Windows = " $var1
-echo "OS Linux   = " $var2
-echo "OS Other   = " $var3
-echo "Environment Production  = " $var4
-echo "Environment Development = " $var5
-echo "Environment Other       = " $var6
-echo ""
-var1="0"
-var2="0"
-var3="0"
-var4="0"
-var5="0"
-var6="0"
+        echo "OS Windows = " $os_windows_count
+        echo "OS Linux   = " $os_linux_count
+        echo "OS Other   = " $os_other_count
+        echo "Environment Production  = " $env_production_count
+        echo "Environment Development = " $env_development_count
+        echo "Environment Other       = " $env_other_count
+        echo ""
+
 done
+
